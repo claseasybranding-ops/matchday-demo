63b4afd1d1e9b1ed0808392a348285b27d6e400f
claseasybranding-ops <clas.easybranding@gmail.com>
2026-03-13 18:31:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,47 @@ import requests, os
 
 app = Flask(__name__)
 
-# Her kan vi senere legge inn ekte API-kall til API-Sports
-def hent_kamper():
-    return [
-        {
-            "id": 1, 
-            "h_navn": "Liverpool", "h_logo": "https://media.api-sports.io/football/teams/40.png",
-            "b_navn": "Chelsea", "b_logo": "https://media.api-sports.io/football/teams/49.png",
-            "h_score": 0, "b_score": 0
-        },
-        {
-            "id": 2, 
-            "h_navn": "Bodø/Glimt", "h_logo": "https://media.api-sports.io/football/teams/933.png",
-            "b_navn": "Fredrikstad", "b_logo": "https://media.api-sports.io/football/teams/3501.png",
-            "h_score": 0, "b_score": 0
-        }
-    ]
+# Din unike nøkkel til fotballdata
+API_KEY = "c06ec6de7644023e13c7b881248ef5bc"
+
+def hent_live_kamper():
+    url = "https://v3.football.api-sports.io/fixtures?live=all"
+    headers = {
+        'x-apisports-key': API_KEY
+    }
+    
+    try:
+        response = requests.get(url, headers=headers).json()
+        kamper = []
+        
+        # Vi henter ut de 3 første live-kampene som et bevis på at det funker
+        for i in range(min(3, len(response['response']))):
+            item = response['response'][i]
+            kamper.append({
+                "id": item['fixture']['id'],
+                "h_navn": item['teams']['home']['name'],
+                "h_logo": item['teams']['home']['logo'],
+                "b_navn": item['teams']['away']['name'],
+                "b_logo": item['teams']['away']['logo'],
+                "h_score": item['goals']['home'],
+                "b_score": item['goals']['away']
+            })
+        
+        # Hvis ingen kamper er live, viser vi Liverpool som backup
+        if not kamper:
+            return [{
+                "id": 1, 
+                "h_navn": "Liverpool", "h_logo": "https://media.api-sports.io/football/teams/40.png",
+                "b_navn": "Chelsea", "b_logo": "https://media.api-sports.io/football/teams/49.png",
+                "h_score": 0, "b_score": 0
+            }]
+        return kamper
+    except:
+        return []
 
 @app.route('/')
 def index():
-    return render_template('index.html', kamper=hent_kamper())
+    return render_template('index.html', kamper=hent_live_kamper())
 
 @app.route('/send_tips', methods=['POST'])
 def send_tips():
